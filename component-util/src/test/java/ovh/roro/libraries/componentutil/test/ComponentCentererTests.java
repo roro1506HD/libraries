61a48dcf7a8e3cdfdab329b5f30b65b0eef6ad11
roro1506HD <roro1506mcpro@gmail.com>
2026-04-18 00:01:32 +0200

    chore(component-util): clean up unit tests

--- component-util/src/test/java/ovh/roro/libraries/componentutil/test/ComponentCentererTests.java
+++ component-util/src/test/java/ovh/roro/libraries/componentutil/test/ComponentCentererTests.java
@@ -4,17 +4,12 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.support.ParameterDeclarations;
 import ovh.roro.libraries.componentutil.ComponentUtil;
 
+import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 class ComponentCentererTests {
 
@@ -23,19 +18,8 @@ class ComponentCentererTests {
         ComponentUtil.loadCustom(Objects.requireNonNull(ComponentCentererTests.class.getResourceAsStream("/font_data"), "font_data not found"));
     }
 
-    @ParameterizedTest(name = "Padding Validation (Normal): \"{0}\"", quoteTextArguments = false)
-    @ArgumentsSource(NormalInputValues.class)
-    void validateNormalSpacePadding(String input, String expectedResult) {
-        Assertions.assertEquals(expectedResult, ComponentUtil.centerVanilla(Component.literal(input)).getString());
-    }
-
-    @ParameterizedTest(name = "Padding Validation (Bold): \"{0}\"", quoteTextArguments = false)
-    @ArgumentsSource(BoldInputValues.class)
-    void validateBoldSpacePadding(String input, String expectedResult) {
-        Assertions.assertEquals(expectedResult, ComponentUtil.centerVanilla(Component.literal(input).withStyle(ChatFormatting.BOLD)).getString());
-    }
-
     @Test
+    @DisplayName("Width validation")
     void validateWidth() {
         Assertions.assertEquals(4.0F, ComponentUtil.widthVanilla(Component.literal(" ")));
         Assertions.assertEquals(5.0F, ComponentUtil.widthVanilla(Component.literal(" ").withStyle(ChatFormatting.BOLD)));
@@ -44,29 +28,29 @@ class ComponentCentererTests {
         Assertions.assertEquals(206.0F, ComponentUtil.widthVanilla(Component.literal("Medium text that takes more space").withStyle(ChatFormatting.BOLD)));
     }
 
-    static class NormalInputValues implements ArgumentsProvider {
+    @Test
+    @DisplayName("Space padding validation")
+    void validateSpacePadding() {
+        Map<String, String> normalValues = Map.of(
+                "", "                                      ",
+                "Short text", "                               Short text",
+                "Medium text that takes more space", "                Medium text that takes more space",
+                "Long text that should have no padding at all since it already covers more than chat width", "Long text that should have no padding at all since it already covers more than chat width"
+        );
 
-        @Override
-        public Stream<? extends Arguments> provideArguments(ParameterDeclarations parameters, ExtensionContext context) throws Exception {
-            return Stream.of(
-                    Arguments.arguments("", "                                      "),
-                    Arguments.arguments("Short text", "                               Short text"),
-                    Arguments.arguments("Medium text that takes more space", "                Medium text that takes more space"),
-                    Arguments.arguments("Long text that should have no padding at all since it already covers more than chat width", "Long text that should have no padding at all since it already covers more than chat width")
-            );
+        Map<String, String> boldValues = Map.of(
+                "", "                                      ",
+                "Short text", "                              Short text",
+                "Medium text that takes more space", "            Medium text that takes more space",
+                "Long text that should have no padding at all since it already covers more than chat width", "Long text that should have no padding at all since it already covers more than chat width"
+        );
+
+        for (Map.Entry<String, String> entry : normalValues.entrySet()) {
+            Assertions.assertEquals(entry.getValue(), ComponentUtil.centerVanilla(Component.literal(entry.getKey())).getString());
         }
-    }
 
-    static class BoldInputValues implements ArgumentsProvider {
-
-        @Override
-        public Stream<? extends Arguments> provideArguments(ParameterDeclarations parameters, ExtensionContext context) throws Exception {
-            return Stream.of(
-                    Arguments.arguments("", "                                      "),
-                    Arguments.arguments("Short text", "                              Short text"),
-                    Arguments.arguments("Medium text that takes more space", "            Medium text that takes more space"),
-                    Arguments.arguments("Long text that should have no padding at all since it already covers more than chat width", "Long text that should have no padding at all since it already covers more than chat width")
-            );
+        for (Map.Entry<String, String> entry : boldValues.entrySet()) {
+            Assertions.assertEquals(entry.getValue(), ComponentUtil.centerVanilla(Component.literal(entry.getKey()).withStyle(ChatFormatting.BOLD)).getString());
         }
     }
 }
